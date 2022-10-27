8d93fc1e27d1e67dfae38fde8227f12a0cbd7e7e
RZhang16 <RZhang16@slb.com>
2022-10-27 11:30:22 +0800

    feat: cool car

---
+++ OOPractice/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPracticeTest
+{
+    public class Car
+    {
+        private string name;
+        private string speed;
+
+        public Car(string name, string speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return $"{name}: speed up {speed} km/h";
+        }
+    }
+}
